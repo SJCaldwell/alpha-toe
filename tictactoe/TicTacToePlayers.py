9601dd3f856c502bf73f985982e69ef1d65ccc5d
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-04-18 23:43:10 -0400

    added tictactoe gui

--- tictactoe/TicTacToePlayers.py
+++ tictactoe/TicTacToePlayers.py
@@ -1,7 +1,8 @@
 import numpy as np
+from builtins import input
 
 """
-Random and Human-ineracting players for the game of TicTacToe.
+Random and Human-interacting players for the game of TicTacToe.
 
 Author: Evgeny Tyurin, github.com/evg-tyurin
 Date: Jan 5, 2018.
@@ -28,16 +29,19 @@ class HumanTicTacToePlayer():
     def play(self, board):
         # display(board)
         valid = self.game.getValidMoves(board, 1)
+        print("Available moves (row, column):")
         for i in range(len(valid)):
             if valid[i]:
                 print(int(i/self.game.n), int(i%self.game.n))
         while True: 
+            
+            print("Enter your move in the form (x,y) (no parentheses): ")
             # Python 3.x
             a = input()
-            # Python 2.x 
+            # Python 2.x
             # a = raw_input()
 
-            x,y = [int(x) for x in a.split(' ')]
+            x,y = [int(x) for x in a.split(',')]
             a = self.game.n * x + y if x!= -1 else self.game.n ** 2
             if valid[a]:
                 break

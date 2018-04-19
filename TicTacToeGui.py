2cab4c3a9455017b154359d950517afa2599122c
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-04-19 02:34:24 -0400

    got reset to work. still a bug where sometimes who you're playing as changes

--- TicTacToeGui.py
+++ TicTacToeGui.py
@@ -36,107 +36,6 @@ args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, nn, args1)
 nnp = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
-'''
-class Board:
-
-  def __init__(self,other=None):
-    self.player = 'X'
-    self.opponent = 'O'
-    self.empty = '.'
-    self.size = 3
-    self.fields = {}
-    for y in range(self.size):
-      for x in range(self.size):
-        self.fields[x,y] = self.empty
-    # copy constructor
-    if other:
-      self.__dict__ = deepcopy(other.__dict__)
-
-  def move(self,x,y):
-    board = Board(self)
-    board.fields[x,y] = board.player
-    (board.player,board.opponent) = (board.opponent,board.player)
-    return board
-
-  def __minimax(self, player):
-    if self.won():
-      if player:
-        return (-1,None)
-      else:
-        return (+1,None)
-    elif self.tied():
-      return (0,None)
-    elif player:
-      best = (-2,None)
-      for x,y in self.fields:
-        if self.fields[x,y]==self.empty:
-          value = self.move(x,y).__minimax(not player)[0]
-          if value>best[0]:
-            best = (value,(x,y))
-      return best
-    else:
-      best = (+2,None)
-      for x,y in self.fields:
-        if self.fields[x,y]==self.empty:
-          value = self.move(x,y).__minimax(not player)[0]
-          if value<best[0]:
-            best = (value,(x,y))
-      return best
-
-  def best(self):
-    return self.__minimax(True)[1]
-
-  def tied(self):
-    for (x,y) in self.fields:
-      if self.fields[x,y]==self.empty:
-        return False
-    return True
-
-  def won(self):
-    # horizontal
-    for y in range(self.size):
-      winning = []
-      for x in range(self.size):
-        if self.fields[x,y] == self.opponent:
-          winning.append((x,y))
-      if len(winning) == self.size:
-        return winning
-    # vertical
-    for x in range(self.size):
-      winning = []
-      for y in range(self.size):
-        if self.fields[x,y] == self.opponent:
-          winning.append((x,y))
-      if len(winning) == self.size:
-        return winning
-    # diagonal
-    winning = []
-    for y in range(self.size):
-      x = y
-      if self.fields[x,y] == self.opponent:
-        winning.append((x,y))
-    if len(winning) == self.size:
-      return winning
-    # other diagonal
-    winning = []
-    for y in range(self.size):
-      x = self.size-1-y
-      if self.fields[x,y] == self.opponent:
-        winning.append((x,y))
-    if len(winning) == self.size:
-      return winning
-    # default
-    return None
-
-  def __str__(self):
-    string = ''
-    for y in range(self.size):
-      for x in range(self.size):
-        string+=self.fields[x,y]
-      string+="\n"
-    return string
-'''
-
 class GUI:
 
   def __init__(self):
@@ -146,9 +45,10 @@ class GUI:
     self.board = g.getInitBoard()
     self.curPlayer = 1
     self.Lagann = ImageTk.PhotoImage(Image.open("mecha.jpg"))
+    self.LagannWin = ImageTk.PhotoImage(Image.open("mechawin.jpg")) # NN will never lose
     self.Killmanovich = ImageTk.PhotoImage(Image.open("killmanovich.png"))
+    self.KillmanovichLose = ImageTk.PhotoImage(Image.open("killmanovichlose.png"))
     self.blankSquare = ImageTk.PhotoImage(Image.open("white square.png"))
-    self.font = Font(family="Helvetica", size=32)
     self.buttons = {}
     for x in range(BOARD_SIZE):
         for y in range(BOARD_SIZE):
@@ -169,12 +69,12 @@ class GUI:
   def move(self,x,y):
     self.app.config(cursor="watch")
     self.app.update()
-    print x, y
+    #print x, y
     self.board, self.curPlayer = g.getNextState(self.board, self.curPlayer, BOARD_SIZE*x + y)
     #self.board = self.board.move(x,y)
     self.update()
     move = nnp(g.getCanonicalForm(self.board, self.curPlayer))
-    print move
+    #print move
     if move:
       self.board, self.curPlayer = g.getNextState(self.board, self.curPlayer, move)
       #self.board = self.board.move(*move)
@@ -186,27 +86,39 @@ class GUI:
         for y in range(BOARD_SIZE):
           piece = self.board[x,y]
           img = self.blankSquare
-          if piece == -1:
-            img = self.Lagann
           if piece == 1:
             img = self.Killmanovich
+          if piece == -1:
+            img = self.Lagann
           self.buttons[x,y].config(image = img)
-          #self.buttons[x,y]['disabledforeground'] = 'black'
-          if piece == 0:
+          if piece == 0:  # enable button
             self.buttons[x,y]['state'] = 'normal'
-          else:
+            self.buttons[x,y]['command'] = lambda x=x,y=y: self.move(x,y)
+          else: # disable button
             self.buttons[x,y]['command'] = 0  # this disables the callback
             self.buttons[x,y]['relief'] = 'sunken'  # makes the button fixed
-            #self.buttons[x,y]['state'] = 'disabled'
+
     winning = g.getGameEnded(self.board, self.curPlayer)  #self.board.won()
     if winning:
-      for x in range(BOARD_SIZE):
-        for y in range(BOARD_SIZE):
-          #self.buttons[x,y]['disabledforeground'] = 'red'
-          self.buttons[x,y]['fg'] = 'red'
-          self.buttons[x,y]['command'] = 0  # this disables the callback
-          self.buttons[x,y]['relief'] = 'sunken'  # makes the button fixed
-          #self.buttons[x,y]['state'] = 'disabled'
+      if winning == 1:
+          print("I LOSE? ERROR DOES NOT COMPUTE I CANNOT LOSE")
+      elif winning > 0:
+          print("DRAW DRAW DRAW DRAW DRAW DRAW DRAW DRAW DRAW DRAW DRAW DRAW")
+      else:
+        print("I WIN! HAHA YOU CANNOT DEFEAT ME!")
+        for x in range(BOARD_SIZE):
+          for y in range(BOARD_SIZE):
+            piece = self.board[x,y]
+            img = self.blankSquare
+            if piece == -1:
+              img = self.LagannWin
+            if piece == 1:
+              img = self.KillmanovichLose
+            self.buttons[x,y].config(image = img)
+
+            self.buttons[x,y]['command'] = 0  # this disables the callback
+            self.buttons[x,y]['relief'] = 'sunken'  # makes the button fixed
+
     for x in range(BOARD_SIZE):
         for y in range(BOARD_SIZE):
           self.buttons[x,y].update()

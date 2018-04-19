741067979f2d887884e0a5666786b500678acfbf
Noah R. Johnson <noah.johnson13@ncf.edu>
2018-04-19 00:56:20 -0400

    Hooked up NN model to gui!

--- TicTacToeGui.py
+++ TicTacToeGui.py
@@ -4,11 +4,39 @@
 # Author: Maurits van der Schee <maurits@vdschee.nl>
 # Repo: https://github.com/mevdschee/python-tictactoe
 
+"""
+Run this file to play manually with any agent.
+"""
+
 from Tkinter import Tk, Button, PhotoImage
 from PIL import ImageTk, Image
 from tkFont import Font
 from copy import deepcopy
 
+
+import numpy as np
+
+from MCTS import MCTS
+
+from utils import *
+
+from tictactoe.TicTacToeGame import TicTacToeGame, display
+from tictactoe.TicTacToePlayers import *
+from tictactoe.tensorflow.NNet import NNetWrapper as NNet
+
+
+BOARD_SIZE = 3
+
+g = TicTacToeGame(BOARD_SIZE)
+
+# nnet players
+nn = NNet(g)
+nn.load_checkpoint('./temp/', 'best.pth.tar')
+args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+mcts1 = MCTS(g, nn, args1)
+nnp = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+'''
 class Board:
 
   def __init__(self,other=None):
@@ -107,6 +135,7 @@ class Board:
         string+=self.fields[x,y]
       string+="\n"
     return string
+'''
 
 class GUI:
 
@@ -114,54 +143,72 @@ class GUI:
     self.app = Tk()
     self.app.title('TicTacToe')
     self.app.resizable(width=False, height=False)
-    self.board = Board()
+    self.board = g.getInitBoard()
+    self.curPlayer = 1
     self.Lagann = ImageTk.PhotoImage(Image.open("mecha.jpg"))
     self.Killmanovich = ImageTk.PhotoImage(Image.open("killmanovich.jpg"))
+    self.blankSquare = ImageTk.PhotoImage(Image.open("white square.png"))
     self.font = Font(family="Helvetica", size=32)
     self.buttons = {}
-    for x,y in self.board.fields:
-      handler = lambda x=x,y=y: self.move(x,y)
-      button = Button(self.app, command=handler, font=self.font, width=300, height=300)
-      button.grid(row=y, column=x)
-      self.buttons[x,y] = button
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+          handler = lambda x=x,y=y: self.move(x,y)
+          button = Button(self.app, command=handler, width=300, height=300)
+          button.grid(row=y, column=x)
+          self.buttons[x,y] = button
     handler = lambda: self.reset()
     button = Button(self.app, text='reset', command=handler)
     button.grid(row=self.board.size+1, column=0, columnspan=self.board.size, sticky="WE")
     self.update()
 
   def reset(self):
-    self.board = Board()
+    self.board = g.getInitBoard()
+    self.curPlayer = 1
     self.update()
 
   def move(self,x,y):
     self.app.config(cursor="watch")
     self.app.update()
-    self.board = self.board.move(x,y)
+    print x, y
+    self.board, self.curPlayer = g.getNextState(self.board, self.curPlayer, BOARD_SIZE*x + y)
+    #self.board = self.board.move(x,y)
     self.update()
-    move = self.board.best()
+    move = nnp(g.getCanonicalForm(self.board, self.curPlayer))
+    print move
     if move:
-      self.board = self.board.move(*move)
+      self.board, self.curPlayer = g.getNextState(self.board, self.curPlayer, move)
+      #self.board = self.board.move(*move)
       self.update()
     self.app.config(cursor="")
 
   def update(self):
-    for (x,y) in self.board.fields:
-      text = self.board.fields[x,y]
-      #self.buttons[x,y]['text'] = text
-      self.buttons[x,y].config(image = self.Killmanovich if text == 'O' else self.Lagann)
-      self.buttons[x,y]['disabledforeground'] = 'black'
-      if text==self.board.empty:
-        self.buttons[x,y]['state'] = 'normal'
-      else:
-        self.buttons[x,y]['state'] = 'disabled'
-    winning = self.board.won()
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+          piece = self.board[x,y]
+          img = self.blankSquare
+          if piece == -1:
+            img = self.Lagann
+          if piece == 1:
+            img = self.Killmanovich
+          self.buttons[x,y].config(image = img)
+          #self.buttons[x,y]['disabledforeground'] = 'black'
+          if piece == 0:
+            self.buttons[x,y]['state'] = 'normal'
+          else:
+            self.buttons[x,y]['command'] = 0  # this disables the callback
+            self.buttons[x,y]['relief'] = 'sunken'  # makes the button fixed
+            #self.buttons[x,y]['state'] = 'disabled'
+    winning = g.getGameEnded(self.board, self.curPlayer)  #self.board.won()
     if winning:
-      for x,y in winning:
-        self.buttons[x,y]['disabledforeground'] = 'red'
+      #for x,y in winning:
+      #  self.buttons[x,y]['disabledforeground'] = 'red'
       for x,y in self.buttons:
-        self.buttons[x,y]['state'] = 'disabled'
-    for (x,y) in self.board.fields:
-      self.buttons[x,y].update()
+        self.buttons[x,y]['command'] = 0  # this disables the callback
+        self.buttons[x,y]['relief'] = 'sunken'  # makes the button fixed
+        #self.buttons[x,y]['state'] = 'disabled'
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+          self.buttons[x,y].update()
 
   def mainloop(self):
     self.app.mainloop()
